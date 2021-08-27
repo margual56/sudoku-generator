551c2c39fcb5fcc721c2b868317004b4fd3c6fd6
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-08-27 22:09:14 +0200

    Added a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import '../styles/footer.scss';
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>
+				This is an <a href="https://opensource.org/docs/osd" title="What is Open Source?" target="_blank" rel="noreferrer">Open Source</a> project
+			</p>
+
+			<p>
+				Check out <a href="https://github.com/margual56/sudoku-generator" title="Repository of this project" target="_blank" rel="noreferrer">the code</a>
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;
