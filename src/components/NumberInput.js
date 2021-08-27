6185e83396958af8c07e0ac6cc926e7c9482df6e
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-08-27 22:33:50 +0200

    Fixed error on empty textarea
    After deleting a value from a textarea, it said the cell had a wrong value

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 
 class NumberInput extends Component {
+	text = "";
+
 	constructor(props){
 		super(props);
 
@@ -19,18 +21,26 @@ class NumberInput extends Component {
 		let hasCorrect = this.state.classList.indexOf("correct");
 		let hasWrong = this.state.classList.indexOf("wrong");
  
-		if(isCorrect){
-			if(hasCorrect === -1)
-				this.state.classList.push("correct");
+		if(this.text === ''){
+			if(hasCorrect !== -1)
+				this.state.classList.splice(hasCorrect, 1);
 
 			if(hasWrong !== -1)
 				this.state.classList.splice(hasWrong, 1);
-		}else{
-			if(hasWrong === -1)
-				this.state.classList.push("wrong");
+		}else {
+			if(isCorrect){
+				if(hasCorrect === -1)
+					this.state.classList.push("correct");
 
-			if(hasCorrect !== -1)
-				this.state.classList.splice(hasCorrect, 1);
+				if(hasWrong !== -1)
+					this.state.classList.splice(hasWrong, 1);
+			}else{
+				if(hasWrong === -1)
+					this.state.classList.push("wrong");
+
+				if(hasCorrect !== -1)
+					this.state.classList.splice(hasCorrect, 1);
+			}
 		}
 
 		this.forceUpdate();
@@ -42,9 +52,14 @@ class NumberInput extends Component {
 				className={this.state.classList.join(" ")}
 				onBlur={(e) => this.checkInput(e)}
 				onKeyPress={(e) => {
-					if(window.isNaN(e.key))
-						e.preventDefault()
+					if(window.isNaN(e.key) || e.key === ' ')
+						e.preventDefault();
+					else
+						this.text = e.key;
+
+					console.log(this.text);
 				}}
+				onClick={e => {e.target.value=""; this.text = "";}}
 				maxLength={1} 
 				cols={1} 
 				rows={1}
